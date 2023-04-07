3a8148848002196b8b86996e954fcb1815fe48ed
AnimalO <o.loginoff@gmail.com>
2023-04-07 11:35:43 +0300

    Пока как бекап.

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -19,6 +20,12 @@ public class MainApp {
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      //my thoughts
+//      Car car1 = new Car("BMW", 6);
+//      Car car2 = new Car("Audi",1);
+//      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("BMW", 6)));
+//      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("Audi",1)));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +33,12 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+         //My
+//         System.out.println(user.getUser_car());
+//         System.out.println();
 
+      }
+//      users.get(0).setUser_car(car1);
       context.close();
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -6,56 +6,78 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   @Column(name = "name")
-   private String firstName;
+    @Column(name = "name")
+    private String firstName;
 
-   @Column(name = "last_name")
-   private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-   @Column(name = "email")
-   private String email;
+    @Column(name = "email")
+    private String email;
+//        @OneToOne (mappedBy = "id")
+//    @MapsId
+//    @JoinColumn(name = "car_id", referencedColumnName = "id")
+//   private Car user_car;
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public User() {
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    //мои тесты
 
-   public void setId(Long id) {
-      this.id = id;
-   }
 
-   public String getFirstName() {
-      return firstName;
-   }
+//    public User(String firstName, String lastName, String email, Car user_car) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.email = email;
+//        this.user_car = user_car;
+//    }
+//
+//    public Car getUser_car() {
+//        return user_car;
+//    }
+//
+//    public void setUser_car(Car user_car) {
+//        this.user_car = user_car;
+//    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
